3cffa10062a25e9a6d06fb9549a2647e03028d69
Mikalai Silivonik <mikalai.silivonik@outlook.com>
2017-03-21 21:09:37 -0600

    updated to support old nodejs versions

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
 'use strict';
 
-const winston = require('winston');
+var winston = require('winston');
 
 
-class Tracer {
-    constructor() {
-        this.winston = winston;
-        this.config = [];
-    }
-
-    get(tag) {
-        const tracer = winston.loggers.add(tag, { console: { level: 'info' } });
-        this.configure(this.config);
-        return tracer;
-    }
-
-    configure(config) {
-        this.config = config || [];
-        const loggerTags = Object.keys(winston.loggers.loggers);
-        loggerTags.forEach(tag => {
-            this.config.forEach(cfg => {
-                const match = tag.match(new RegExp(cfg.pattern, 'i'));
-                if (match && match.length) {
-                    if (!cfg.getOptions) {
-                        throw new Error('Tracer configuration doesn\'t have "getOptions(tag)" function');
-                    }
-                    const config = cfg.getOptions(tag);
-                    if (!config) {
-                        throw new Error(`"getOptions('${tag}')" returned unexpected value`);
-                    }
-                    winston.loggers.loggers[tag].configure(config);
-                }
-            });
-        });
-    }
+function Tracer() {
+    this.winston = winston;
+    this.config = [];
 }
+
+Tracer.prototype.get = function(tag) {
+    var tracer = winston.loggers.add(tag, { console: { level: 'info' } });
+    this.configure(this.config);
+    return tracer;
+};
+
+Tracer.prototype.configure = function(config) {
+    this.config = config || [];
+    var loggerTags = Object.keys(winston.loggers.loggers);
+    loggerTags.forEach(tag => {
+        this.config.forEach(cfg => {
+            var match = tag.match(new RegExp(cfg.pattern, 'i'));
+            if (match && match.length) {
+                if (!cfg.getOptions) {
+                    throw new Error('Tracer configuration doesn\'t have "getOptions(tag)" function');
+                }
+                var config = cfg.getOptions(tag);
+                if (!config) {
+                    throw new Error(`"getOptions('${tag}')" returned unexpected value`);
+                }
+                winston.loggers.loggers[tag].configure(config);
+            }
+        });
+    });
+};
 
 
 module.exports = new Tracer();
